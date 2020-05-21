4fa0501caf17f129ffe92813a40eda7ddf64d4e3
AVData <AVData@users.noreply.github.com>
2020-05-21 14:07:24 -0700

    improved some code with lecture notes.  Need to complete mvp

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,41 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+
+def single_number(nums):
+    # Your code here
+    # arr.sort()
+    # print(arr)
+    # for k, v in enumerate(arr):
+    #     if arr[k] == True:
+    #         continue
+    #     elif arr[k + 1] == v:
+    #         print(v, arr[k])
+    #     else:
+    #         print(arr[k])
+
+    #
+    # first-pass solution
+    # we'll keep an array, call it `no_dups` to hold numbers we see in the nums
+    # array
+    no_dups = []
+    # iterate through nums
+    # using an array to hold the dups and then searching through it
+    # one thing that arrays are not great are is searching for a particular
+    # value in the worst case, that's going to be O(n)
+    # what are other data structures that better suited to being searched?
+    for x in nums:  # O(n)
+        # check to see if the number is already in the `no_dups` array
+        if x not in no_dups:  # O(n)
+            no_dups.append(x)
+        # if it is, remove it from the `no_dups` array
+        else:
+            no_dups.remove(x)  # O(n)
+    # when we're done iterating, the only number in our `no_dups` array is the
+    # odd number out
+    # return it
+    return no_dups[0]
 
 
 if __name__ == '__main__':
